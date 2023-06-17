61418f6fbde18552acccf798ffe317dc7b739cda
Nunyaklah <nunyaklah@gmail.com>
2023-06-17 21:42:55 +0000

    added banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,11 +9,25 @@
       <div
         class="rounded-3xl absolute inset-0 flex flex-col justify-start bg-gradient-to-r from-black to-transparent"
       >
-        <div class="ml-6 pt-6">
+        <div class="ml-12 pt-12">
           <h1 class="text-white font-bold text-5xl">Spider Man</h1>
-          <p class="text-white font-semi text-3xl pt-2">Into the Spider Verse</p>
+          <p class="text-white font-semi text-3xl pt-2">
+            Into the Spider Verse
+          </p>
+        </div>
+        <div class="ml-12 mt-14 flex  items-center">
+          <i class="ri-netflix-fill text-red-800 text-xl"></i>
+          <p class="text-xs text-white font-semibold ml-2">8.4</p>
+          <i class="ri-translate-2 text-white text-xl ml-6"></i>
+          <p class="text-xs text-white font-semibold ml-2">English</p>
+        </div>
+        <div class="ml-12 mt-14">
+          <Button />
         </div>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import Button from "./Button.vue";
+</script>
---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center mr-2"
+    >
+      <i class="ri-movie-2-line text-xl mr-3"></i>
+      Watch now
+    </button>
+  </div>
+</template>
